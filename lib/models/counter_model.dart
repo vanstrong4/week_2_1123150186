196166fc6cbc55abd94be8d89e2302331a46267f
vanstrong4 <alfeushendrievan@gmail.com>
2026-02-26 08:25:55 +0700

    tambahin notifylisteners

--- lib/models/counter_model.dart
+++ lib/models/counter_model.dart
@@ -1,14 +1,17 @@
-class CounterModel {
+import 'package:flutter/material.dart';
+class CounterModel extends ChangeNotifier{
   int _angka = 0;
   get angka => _angka;
 
   void increment(){
     _angka++;
+    notifyListeners();
   }
 
   void kurang(){
     if(_angka > 0){
       _angka--;
+      notifyListeners();
     }
   }
 }
