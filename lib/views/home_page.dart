597a4e192ac90055f654482c7b55851a37f9a05f
vanstrong4 <alfeushendrievan@gmail.com>
2026-02-26 08:33:50 +0700

    tambah function increment dari CounterModel

--- lib/views/home_page.dart
+++ lib/views/home_page.dart
@@ -28,14 +28,16 @@ class _MyHomePageState extends State<MyHomePage> {
               children: <Widget>[
                 const Text('You have pushed the button this many times:'),
                 Text(
-                  '$_counter',
+                  value.angka.toString(),
                   style: Theme.of(context).textTheme.headlineMedium,
                 ),
               ],
             ),
           ),
           floatingActionButton: FloatingActionButton(
-            onPressed: ,
+            onPressed: (){
+              value.increment();
+            },
             tooltip: 'Increment',
             child: const Icon(Icons.add),
           ),
