2a68479b677e98495625d3fec22eefa9e4838082
vanstrong4 <alfeushendrievan@gmail.com>
2026-02-26 08:29:28 +0700

    tamabahin changenotifierProvider di main

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:week_2_1123150186/models/counter_model.dart';
 import 'package:week_2_1123150186/views/home_page.dart';
+import 'package:provider/provider.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(ChangeNotifierProvider(
+    create: (context) => CounterModel(),
+    child: MyApp()
+  ));
 }
 
 class MyApp extends StatelessWidget {
